1daadc77f0b5ae9a2cb5cd0339298a3776f932e9
ColdThunder11 <34985592+ColdThunder11@users.noreply.github.com>
2021-11-03 17:38:08 +0800

    Create shindan.py

---
+++ shindan.py
@@ -0,0 +1,40 @@
+from os import path
+from hoshino import Service, priv
+import traceback
+from typing import *
+from playwright.async_api import async_playwright
+
+
+sv = Service('PCR女友',manage_priv=priv.SUPERUSER)
+
+cache_path = path.join(path.abspath(path.dirname(__file__)),"cache")
+
+@sv.on_fullmatch(('PCR女友','pcr女友'))
+async def get_pcr_shindan(bot,ev):
+    await bot.send(ev,f"正在生成中，请稍后")
+    if "card" in ev.sender.keys():
+        user_name = ev.sender["card"] 
+    else:
+        user_name = ev.sender["nickname"] 
+    try:
+        async with async_playwright() as p:
+            browser = await p.chromium.launch()
+            context = await browser.new_context(viewport={"width":720, "height":1080})
+            page = await context.new_page()
+            await page.goto("https://shindan.priconne-redive.jp/")
+            await page.click("input[type=\"text\"]")
+            await page.fill("input[type=\"text\"]",user_name)
+            await page.click("button")
+            await page.wait_for_load_state("networkidle")
+            div = await page.query_selector("#app > main > div > div > div")
+            div = await div.bounding_box()
+            img = await page.screenshot(clip=div,path=path.join(cache_path,f"{user_name}.png"),full_page=True)
+            await page.close()
+            await context.close()
+            await browser.close()
+            img_path = path.join(cache_path,f"{user_name}.png")
+    except:
+        await bot.send(ev,f"图片生成失败了")
+        traceback.print_exc()
+        return
+    await bot.send(ev,f"[CQ:image,file=file:///{img_path}]")
